d081c466889b1983281b41d9eddd2e76e9152289
marcelaOrellana <marcelapaz.1985@gmail.com>
2017-05-09 12:06:57 -0300

    ejercicio 2 guiado

--- programacion-funcional-01.js
+++ programacion-funcional-01.js
@@ -1,5 +1,7 @@
+//Ejercicio 1
 l1 = [1,2,3,5,6];
 l2 = [2,3,4,5,7];
+//Forma Imperativa
 function intersecion(l1, l2){
 	var l3 = [];
 	for(var i = 0; i < l1.length; i++){
@@ -12,12 +14,46 @@ function intersecion(l1, l2){
 	console.log(l3);
 	return l3;	
 }
-
+//Forma declarativa
 function intersectionSet2(){
 	return l1.filter(function(n){
 		return l2.indexOf(n) !== -1;
 	});
 }
 
+//Ejercicio 2
+var productos = [
+	{
+		nombre: "pijama", 
+		tipo: "ropa",
+	},
+	{
+		nombre: "nevera", 
+		tipo: "electrodomesticos",
+	},
+	{
+		nombre: "macbook", 
+		tipo: "computador",
+	},
+	{
+		nombre: "pijama", 
+		tipo: "ropa",
+	},
+	{
+		nombre: "toshiba", 
+		tipo: "computador",
+	}
+];
 
+//Forma Imperativa
+var computadores = [];
+for(var i = 0; i < productos.length; i++){
+	if(productos[i].tipo == "computador"){
+		computadores.push(productos[i]);
+	}
+}
 
+//Forma Declarativa
+var computadores = productos.filter(function(productos){
+	return productos.tipo === "computador";
+});
